909f750b46b4e3d803b834c4b0f83f4fc563b221
dinithimendis <dinithimendis42@gmail.com>
2023-11-28 21:58:46 +0530

    add all js

--- src/lk/ijse/cal/Script.js
+++ src/lk/ijse/cal/Script.js
@@ -4,3 +4,60 @@ var weight = document.getElementById("weight");
 var male = document.getElementById("m");
 var female = document.getElementById("f");
 var form = document.getElementById("form");
+
+function validation() {
+    if (
+        age.value == "" ||
+        height.value == "" ||
+        weight.value == "" ||
+        (male.checked == false && female.checked == false)
+    ) {
+        alert("All fields are required!");
+        document.getElementById("submit").removeEventListener("click", count);
+    } else {
+        count();
+    }
+}
+
+document.getElementById("submit").addEventListener("click", validation);
+
+function count() {
+    var p = [age.value, height.value, weight.value];
+    if (male.checked) {
+        p.push("male");
+    } else if (female.checked) {
+        p.push("female");
+    }
+    form.reset();
+    var bmi = Number(p[2]) / (((Number(p[1]) / 100) * Number(p[1])) / 100);
+    var result = "";
+    if (bmi < 18.5) {
+        result = "low!!!";
+    } else if (18.5 <= bmi && bmi <= 24.9) {
+        result = "Healthy";
+    } else if (25 <= bmi && bmi <= 29.9) {
+        result = "Over!!!";
+    } else if (30 <= bmi && bmi <= 34.9) {
+        result = "Danger";
+    } else {
+        result = "Extremely Danger";
+    }
+}
+
+var h1 = document.createElement("h1");
+var h2 = document.createElement("h2");
+var t = document.createTextNode(result);
+var b = document.createTextNode("BMI: ");
+var r = document.createTextNode(parseFloat(bmi).toFixed(2));
+
+h1.appendChild(t);
+h2.appendChild(b);
+h2.appendChild(r);
+document.body.appendChild(h1);
+document.body.appendChild(h2);
+
+document.getElementById("submit").removeEventListener("click", count);
+
+document.getElementById("submit").removeEventListener("click", validation);
+
+document.getElementById("submit").addEventListener("click", count);
