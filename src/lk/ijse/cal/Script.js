79b7e0bc7e98019375f1eaabae166a5b6badb33d
dinithimendis <dinithimendis42@gmail.com>
2023-11-28 21:41:25 +0530

    add js

---
+++ src/lk/ijse/cal/Script.js
@@ -0,0 +1,6 @@
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var male = document.getElementById("m");
+var female = document.getElementById("f");
+var form = document.getElementById("form");
